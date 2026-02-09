e9b389bb7dd061d35bc0d4cf88f1975630c32bb2
kdubey12 <dubeyk@stpaulsschool.org.uk>
2026-02-09 10:01:57 +0000

    Ex. 1-26

---
+++ OOPDraw/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDraw
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(Pen p, int x1, int y1, int x2, int y2) : base(p, x1, y1, x2, y2) { }
+
+        public Rectangle(Pen p, int x1, int y1) : base(p, x1, y1) { }
+
+        public override void Draw(Graphics g)
+        {
+            (int x, int y, int w, int h) = EnclosingRectangle();
+            g.DrawRectangle(Pen, x, y, w, h);
+        }
+
+        public bool FullySurrounds(Shape s)
+        {
+            (int x, int y, int w, int h) = this.EnclosingRectangle();
+            (int xs, int ys, int ws, int hs) = s.EnclosingRectangle();
+
+            return x < xs && y < ys && x + w > xs + ws && y + h > ys + hs;
+        }
+    }
+}
---
+++ OOPDraw/Shape.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDraw
+{
+    public abstract class Shape
+    {
+        public Pen Pen { get; protected set; }
+        public int X1 { get; protected set; }
+        public int Y1 { get; protected set; }
+        public int X2 { get; protected set; }
+        public int Y2 { get; protected set; }
+
+        public bool Selected { get; private set; }
+
+        public Shape(Pen p, int x1, int y1, int x2, int y2)
+        {
+            Pen = new Pen(p.Color, p.Width);
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public Shape(Pen p, int x1, int y1) : this(p, x1, y1, x1, y1) { }
+
+        public abstract void Draw(Graphics g);
+
+        public virtual void GrowTo(int x2, int y2)
+        {
+            X2 = x2;
+            Y2 = y2;
+        }
+
+        public (int, int, int, int) EnclosingRectangle()
+        {
+            int x = Math.Min(X1, X2);
+            int y = Math.Min(Y1, Y2);
+            int w = Math.Max(X1, X2) - x;
+            int h = Math.Max(Y1, Y2) - y;
+            return (x, y, w, h);
+        }
+
+        public void MoveBy(int xDelta, int yDelta)
+        {
+            X1 += xDelta;
+            Y1 += yDelta;
+            X2 += xDelta;
+            Y2 += yDelta;
+        }
+
+        public void Select()
+        {
+            Selected = true;
+            Pen.DashStyle = DashStyle.Dash;
+        }
+
+        public void Deselect()
+        {
+            Selected = false;
+            Pen.DashStyle = DashStyle.Solid;
+        }
+    }
+}
